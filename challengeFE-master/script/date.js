1975a6d684acab22a27459d5253a4bc6a7826252
Gustavo Verissimo <126837328+vrssm@users.noreply.github.com>
2023-05-22 00:07:19 -0300

    Atualização Header + Dark mode + add Footer
    Header atualizado com Dark Mode 
    Adicionado Botao Dark e light
    Adicionado footer

--- challengeFE-master/script/date.js
+++ challengeFE-master/script/date.js
@@ -3,3 +3,14 @@ document.write(horas.getHours());
 document.write(':')
 let minutos = new Date()
 document.write(minutos.getMinutes());
+
+// Data para o footer
+
+date = document.getElementById('date');
+let data = new Date();
+let dia = ('0' + data.getDate()).slice(-2);
+let mes = data.getMonth();
+let meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+let ano = data.getFullYear()
+
+date.innerHTML += `<p id="date"> ${dia} de ${meses[mes]} de ${ano} <\p>`
